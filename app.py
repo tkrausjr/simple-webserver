c15e198e99d12775979dc00ecdd0fc4d3fb566ba
Thomas US1072108 <kraust@kraust-m01.vmware.com>
2017-10-02 14:42:10 -0400

    Added changed setup port

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def menu():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-  app.run(debug=True,host='0.0.0.0', port=5000)
+  app.run(debug=True,host='0.0.0.0', port=5500)
 
