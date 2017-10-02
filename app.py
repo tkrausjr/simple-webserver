6cde8b08f489b2c6792df96a1a64f92cab814ef9
Thomas US1072108 <kraust@kraust-m01.vmware.com>
2017-10-02 14:04:58 -0400

    Added and tested simple webpage

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+# app.route below will result in :
+# the webserver page being served at http:<IP Address>:<port>/menu
+
+@app.route('/menu')
+
+def menu():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True,host='0.0.0.0', port=5000)
+
